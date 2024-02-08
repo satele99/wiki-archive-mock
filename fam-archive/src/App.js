4cc7f97fdb8cae3c18156a2d7cfede70f24774f7
satele99 <satele.works@gmail.com>
2024-02-08 04:13:07 -0500

    new react app

---
+++ fam-archive/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './components/SIdebar';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ fam-archive/src/components/SIdebar.jsx
@@ -0,0 +1,7 @@
+import '../styles/Sidebar.module.css'
+
+export default function Sidebar(){
+    return (
+        <h1>Hello</h1>
+    )
+}
